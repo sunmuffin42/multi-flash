df02903b9fca4bf2625457c599d8c18a02df7230
Edward Butler <edwardbutler@edwards-mbp.ad.bcm.edu>
2017-06-13 16:41:14 -0500

    Add component that renders card input items dynamically

--- imports/ui/CardInputEntries.js
+++ imports/ui/CardInputEntries.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import MainNavBar from './MainNavBar';
+import CardInputItem from './CardInputItem'
 
 export default class CardInputEntries extends React.Component{
+  renderCards(){
+    var list = [];
+    let count = this.props.count;
+    for (var i = 0; i < count; i++){
+      list.push(<CardInputItem/>);
+    }
+    return list;
+  }
   render(){
     return (
       <div>
         <p>Populate {this.props.count} cards.</p>
+        {this.renderCards()}
       </div>
     );
   }
---
+++ imports/ui/CardInputItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainNavBar from './MainNavBar';
+
+export default class CardInputEntries extends React.Component{
+  render(){
+    return (
+      <div>
+        <input type="text"></input>
+        <input type="text"></input>
+        <input type="text"></input>
+      </div>
+    );
+  }
+}
